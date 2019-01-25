36bd66186e58814b2cf778dd9e7348e78af2e651
Van Sang Nguyen <vansang.nguyen96@gmail.com>
2019-01-25 21:45:31 +0700

    Bai1

---
+++ AutoControlAppPC/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoControlAppPC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Process.Start(AppDomain.CurrentDomain.BaseDirectory + "How Kteam - Free Education.html");
+            Process.Start("How Kteam - Free Education.html");
+        }
+    }
+}
