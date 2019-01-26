68d7686b66727c4ecc2c2c400a18f491c8bbcc05
Van Sang Nguyen <vansang.nguyen96@gmail.com>
2019-01-26 13:30:31 +0700

    Bai2

--- AutoControlAppPC/Form1.cs
+++ AutoControlAppPC/Form1.cs
@@ -28,5 +28,66 @@ namespace AutoControlAppPC
             //Process.Start(AppDomain.CurrentDomain.BaseDirectory + "How Kteam - Free Education.html");
             Process.Start("How Kteam - Free Education.html");
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            strCmdText = "/C ping -t howkteam.com";
+            Process.Start("CMD.exe",strCmdText);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string strCmdText;
+            strCmdText = @"/C ""How Kteam - Free Education.html""";
+
+            Process p = new Process();
+
+            p.StartInfo.FileName = "CMD.exe";
+            p.StartInfo.Arguments = strCmdText;
+
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            p.Start();
+
+            //p.Kill();
+
+            /*
+
+           System.Diagnostics.Process process = new System.Diagnostics.Process();
+           System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+           startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+           startInfo.FileName = "cmd.exe";
+           startInfo.Arguments = @"/C ""How Kteam - Free Education.html""";
+           process.StartInfo = startInfo;
+           process.Start();
+
+            */
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string cmdCommand = "ping howkteam.com";
+
+            Process cmd = new Process();
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "CMD.exe";
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardOutput = true;
+
+            cmd.StartInfo = startInfo;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine(cmdCommand);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+
+            string result = cmd.StandardOutput.ReadToEnd();
+            MessageBox.Show(result);
+        }
     }
 }
